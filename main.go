d55f5d9e81d3c298a61d774507af1635aeb1e9bd
FxFx <vinhhp@vhttech.com>
2024-01-11 14:50:56 +0700

    123

--- main.go
+++ main.go
@@ -3,78 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
-
-	log "github.com/inconshreveable/log15"
-
-	"github.com/lemonade-command/lemonade/client"
-	"github.com/lemonade-command/lemonade/lemon"
-	"github.com/lemonade-command/lemonade/server"
 )
 
-var logLevelMap = map[int]log.Lvl{
-	0: log.LvlDebug,
-	1: log.LvlInfo,
-	2: log.LvlWarn,
-	3: log.LvlError,
-	4: log.LvlCrit,
-}
-
 func main() {
-
-	cli := &lemon.CLI{
-		In:  os.Stdin,
-		Out: os.Stdout,
-		Err: os.Stderr,
-	}
-	os.Exit(Do(cli, os.Args))
-}
-
-func Do(c *lemon.CLI, args []string) int {
-	logger := log.New()
-	logger.SetHandler(log.LvlFilterHandler(log.LvlError, log.StdoutHandler))
-
-	if err := c.FlagParse(args, false); err != nil {
-		writeError(c, err)
-		return lemon.FlagParseError
+	if len(os.Args) < 2 {
+		return
 	}
 
-	logLevel := logLevelMap[c.LogLevel]
-	logger.SetHandler(log.LvlFilterHandler(logLevel, log.StdoutHandler))
-
-	if c.Help {
-		fmt.Fprint(c.Err, lemon.Usage)
-		return lemon.Help
-	}
-
-	lc := client.New(c, logger)
-	var err error
-
-	switch c.Type {
-	case lemon.OPEN:
-		logger.Debug("Opening URL")
-		err = lc.Open(c.DataSource, c.TransLocalfile, c.TransLoopback)
-	case lemon.COPY:
-		logger.Debug("Copying text")
-		err = lc.Copy(c.DataSource)
-	case lemon.PASTE:
-		logger.Debug("Pasting text")
-		var text string
-		text, err = lc.Paste()
-		c.Out.Write([]byte(text))
-	case lemon.SERVER:
-		logger.Debug("Starting Server")
-		err = server.Serve(c, logger)
+	Command := os.Args[1]
+	switch Command {
+	case "copy":
+		return
+	case "paste":
+		fmt.Printf("cmm")
+	case "serve":
 	default:
-		panic("Unreachable code")
 	}
-
-	if err != nil {
-		writeError(c, err)
-		return lemon.RPCError
-	}
-	return lemon.Success
-}
-
-func writeError(c *lemon.CLI, err error) {
-	fmt.Fprintln(c.Err, err.Error())
 }
